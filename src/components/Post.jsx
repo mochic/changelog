697900fec0d561df987f4f8df05ebe120d82fccf
christopher mochizuki <mochic808@gmail.com>
2018-11-13 14:21:19 -0800

    update temporary blogging solution

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Post = ({ title, content }) => (
+const Post = ({ title, content, contentList }) => (
   <>
     <h3>{title}</h3>
     <hr />
-    <p>{content}</p>
+    {contentList.length > 0 ? (
+      <ul>
+        {contentList.map(content => (
+          <li>{content}</li>
+        ))}
+      </ul>
+    ) : (
+      <p>{content}</p>
+    )}
   </>
 );
 
 Post.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
+  contentList: PropTypes.arrayOf(PropTypes.string),
 };
 
 Post.defaultProps = {
   title: '',
   content: '',
+  contentList: [],
 };
 
 export default Post;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,11 +3,22 @@ import Post from '../components/Post';
 
 import './styles/App.css';
 
+// this is fine for now...we need to be able to blog about making the blog...
+const CONTENT_LIST_011 = [
+  'update temporary blogging solution to better blog about making the blog...',
+];
+
+const POSTS = [
+  { title: '0.1.0 (2018-11-13T20:58:39+0000)', content: 'bootstrapped project...' },
+  { title: '0.1.1', contentList: CONTENT_LIST_011 },
+].reverse(); // changelog entries are typically ordered in descending order
+
 export default () => (
   <div className="App">
     <h1>changelog</h1>
     <hr />
-    <Post title="0.1.0 (2018-11-13T20:58:39+0000)" content="bootstrapped project..." />
-    <ul />
+    {POSTS.map(post => (
+      <Post {...post} />
+    ))}
   </div>
 );
